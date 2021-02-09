95529358d25cb13028c0a8b3230203e6b163d7f0
dohye1 <tsc03018@gmail.com>
2021-02-09 17:45:00 +0900

    env : webpack에 typescript 설정 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
@@ -9,7 +9,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js)x?$/,
+        test: /\.(js|ts)x?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
@@ -18,7 +18,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.jsx', '.js']
+    extensions: ['.tsx', 'ts', '.jsx', '.js']
   },
   devServer: {
     contentBase: path.resolve(__dirname, './dist')
