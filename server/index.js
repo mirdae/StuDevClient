907b316656ad8fda7f0e56e81c214459c6266192
dohye1 <tsc03018@gmail.com>
2021-02-09 18:26:18 +0900

    env: 여러 dependencies 설치 및 서버 생성

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on PORT  : ${PORT}`));
