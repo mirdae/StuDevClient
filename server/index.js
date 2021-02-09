ae158bfef8105f8d60b9eaf1295542e0ac7de065
dohye1 <tsc03018@gmail.com>
2021-02-09 18:40:36 +0900

    env : mysql db 연결(config 설정 및 dependency 설치)

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
+import './db';
+
+dotenv.config();
 const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
