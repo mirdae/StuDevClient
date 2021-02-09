38377404d0171e4255dcd1c8552d3443d283f1d7
dohye1 <tsc03018@gmail.com>
2021-02-09 17:08:35 +0900

    env : babel config 파일 설정 및 webpack내에 js나 jsx파일 트랜스파일설정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
   devServer: {
     contentBase: path.resolve(__dirname, './dist')
   }
