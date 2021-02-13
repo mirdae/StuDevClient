09f5ff0388f2ebd77a299c1373f5189a51c03f00
dohye1 <tsc03018@gmail.com>
2021-02-13 12:59:40 +0900

    env : global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import AppRouter from './routes'
+import GlobalStyle from './styles/GlobalStyle'
 
 export const App: React.FC<{}> = () => (
   <>
+    <GlobalStyle />
     <AppRouter />
   </>
 )
