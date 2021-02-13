5d005f0e27289e10bcd6ec2a01674aeef4fc4409
dohye1 <tsc03018@gmail.com>
2021-02-13 12:59:15 +0900

    env : color palette 생성

---
+++ src/styles/color-variables.ts
@@ -0,0 +1,17 @@
+export interface Palette {
+  BACKGROUND: string
+  POINT: string
+  WHITE: string
+  INPUT_BORDER: string
+  INPUT_TEXT: string
+}
+
+const PALETTE: Palette = {
+  BACKGROUND: '#F6F6F6',
+  POINT: '#F27C0F',
+  WHITE: '#ffffff',
+  INPUT_BORDER: '#E8E8E8',
+  INPUT_TEXT: '#CDCDCD',
+}
+
+export default PALETTE
