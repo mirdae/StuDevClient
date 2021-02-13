d0ca1906be82073461c63046285d0aa6f91b4170
dohye1 <tsc03018@gmail.com>
2021-02-13 11:34:34 +0900

    [#8] env : index파일에 redux 연결하기

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { Provider } from 'react-redux'
+//import * as store from './store'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { applyMiddleware, createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import rootReducer, { rootSaga } from './modules'
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+)
+
+sagaMiddleware.run(rootSaga)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
