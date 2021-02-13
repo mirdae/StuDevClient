5f1dc0e7b3f47cb6e88b53a263ea959ffb48055f
dohye1 <tsc03018@gmail.com>
2021-02-13 19:55:20 +0900

    style : icon을 보여주는 방식 변경

--- src/containers/SignInForm/SignInForm.tsx
+++ src/containers/SignInForm/SignInForm.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './SignInFormStyle'
 import { Input } from '../../element/Input/index'
 import { Button } from '../../element/Button'
 import { Link } from 'react-router-dom'
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 
 function SignInForm() {
+  const [socialId, setSocialId] = useState('')
+  const [password, setPassword] = useState('')
   const handleSignIn = () => {}
 
   return (
     <S.Container>
-      <Input placeHolder="아이디" icon="assignment_ind" />
-      <Input placeHolder="비밀번호" icon="lock" />
+      <Input
+        type="text"
+        placeHolder="아이디"
+        value={socialId}
+        handleChange={(e) => setSocialId(e.target.value)}
+      >
+        <AccountCircleOutlinedIcon />
+      </Input>
+      <Input
+        type="password"
+        placeHolder="비밀번호"
+        value={password}
+        handleChange={(e) => setPassword(e.target.value)}
+      >
+        <LockOutlinedIcon />
+      </Input>
       <Button text="로그인" handleClick={handleSignIn} type="submit" />
       <Link to="/sign-up">회원가입</Link>
     </S.Container>
--- src/containers/SignUpForm/SignUpForm.tsx
+++ src/containers/SignUpForm/SignUpForm.tsx
@@ -1,19 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './SignUpFormStyle'
 import { Input } from '../../element/Input/index'
 import { Button } from '../../element/Button'
 import { Link } from 'react-router-dom'
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined'
 
 function SignUpForm() {
-  const handleSignUp = () => {}
+  const [socialId, setSocialId] = useState('')
+  const [password, setPassword] = useState('')
+  const [checkPassword, setCheckPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [nickname, setNickname] = useState('')
+
+  const handleSignUp = () => {
+    console.log({ socialId, password, email, nickname })
+  }
 
   return (
     <S.Container>
-      <Input placeHolder="아이디" icon="assignment_ind" />
-      <Input placeHolder="비밀번호" icon="lock" />
-      <Input placeHolder="비밀번호 재확인" icon="lock" />
-      <Input placeHolder="이메일" icon="email" />
-      <Input placeHolder="닉네임" icon="person" />
+      <Input
+        type="text"
+        placeHolder="아이디"
+        value={socialId}
+        handleChange={(e) => setSocialId(e.target.value)}
+      >
+        <AccountCircleOutlinedIcon />
+      </Input>
+      <Input
+        type="password"
+        placeHolder="비밀번호"
+        value={password}
+        handleChange={(e) => setPassword(e.target.value)}
+      >
+        <LockOutlinedIcon />
+      </Input>
+      <Input
+        type="password"
+        placeHolder="비밀번호 재확인"
+        value={checkPassword}
+        handleChange={(e) => setCheckPassword(e.target.value)}
+      >
+        <LockOutlinedIcon />
+      </Input>
+      <Input
+        type="text"
+        placeHolder="이메일"
+        value={email}
+        handleChange={(e) => setEmail(e.target.value)}
+      >
+        <EmailOutlinedIcon />
+      </Input>
+      <Input
+        type="text"
+        placeHolder="닉네임"
+        value={nickname}
+        handleChange={(e) => setNickname(e.target.value)}
+      >
+        <AccountCircleOutlinedIcon />
+      </Input>
       <Button text="회원가입" handleClick={handleSignUp} type="submit" />
       <Link to="/sign-in">로그인</Link>
     </S.Container>
