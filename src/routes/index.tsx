3fc2af5dc6fcbe5564cb5fa5be725f598c4b9723
dohye1 <tsc03018@gmail.com>
2021-02-13 16:46:29 +0900

    feat : route 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Main } from '../pages/Main'
+import { SignUp } from '../pages/SignUp'
+import { SignIn } from '../pages/SignIn'
 
 function AppRouter() {
   return (
     <Switch>
       <Route path="/" exact component={Main} />
+      <Route path="/sign-up" component={SignUp} />
+      <Route path="/sign-in" component={SignIn} />
     </Switch>
   )
 }
