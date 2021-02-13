328406783d5686a31fea5fe1e670f4b087b7b809
dohye1 <tsc03018@gmail.com>
2021-02-13 11:33:21 +0900

    env : ts와 eslint 설정파일 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,26 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': [0],
     'prettier/prettier': 0,
     'react/prop-types': [0],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          String: false,
+          Boolean: false,
+          Number: false,
+          Symbol: false,
+          '{}': false,
+          Object: false,
+          object: false,
+          Function: false,
+        },
+        extendDefaults: true,
+      },
+    ],
   },
 }
