b277890bdb6170cdbdb0070547b66b4f1e7effc8
dohye1 <tsc03018@gmail.com>
2021-02-15 21:29:59 +0900

    feat : 로그인과 회원가입을 위한 사가함수 세팅

--- src/modules/user/saga.ts
+++ src/modules/user/saga.ts
@@ -1,11 +1,42 @@
 import { takeLatest, call, put } from 'redux-saga/effects'
 import AT from './actionTypes'
 import { UserAction } from './types'
+import API from '../../api'
 
-function* getUserInfo(payload: UserAction) {
-  yield console.log(payload)
+function* signUpSaga({ payload }: any) {
+  try {
+    const data = yield call(API.User.signUp, payload)
+    yield put({
+      type: AT.REQUEST_SIGN_UP_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    yield put({
+      type: AT.REQUEST_SIGN_UP_ERROR,
+      payload: error,
+      error: true,
+    })
+  }
+}
+
+function* signInSaga({ payload }: any) {
+  try {
+    const data = yield call(API.User.signIn, payload)
+    console.log(data)
+    yield put({
+      type: AT.REQUEST_SIGN_IN_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    yield put({
+      type: AT.REQUEST_SIGN_IN_ERROR,
+      payload: error,
+      error: true,
+    })
+  }
 }
 
 export function* userSaga() {
-  yield takeLatest(AT.REQUEST_GET_USER_INFO, getUserInfo)
+  yield takeLatest(AT.REQUEST_SIGN_UP, signUpSaga)
+  yield takeLatest(AT.REQUEST_SIGN_IN, signInSaga)
 }
