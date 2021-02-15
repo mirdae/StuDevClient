d9c15c4fde2877f2f8b626dda2994546a2b2bcda
dohye1 <tsc03018@gmail.com>
2021-02-15 23:38:55 +0900

    [#9] feat : createPost페이지 route설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import { Main } from '../pages/Main'
 import { SignUp } from '../pages/SignUp'
 import { SignIn } from '../pages/SignIn'
+import { CreatePost } from '../pages/CreatePost'
 
 function AppRouter() {
   return (
@@ -10,6 +11,7 @@ function AppRouter() {
       <Route path="/" exact component={Main} />
       <Route path="/sign-up" component={SignUp} />
       <Route path="/sign-in" component={SignIn} />
+      <Route path="/create" component={CreatePost} />
     </Switch>
   )
 }
