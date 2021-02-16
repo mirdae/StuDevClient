50543caef8b5f5394d519f425b5bab1f784251de
dohye1 <tsc03018@gmail.com>
2021-02-16 21:35:58 +0900

    feat : postButton container

---
+++ src/containers/PostButton/PostButtonStyle.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import PALETTE from '../../styles/color-variables'
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
---
+++ src/containers/PostButton/index.ts
@@ -0,0 +1,1 @@
+export { default as PostButton } from './PostButton'
