c053c8503ec79608951136c6b50c6d2d93fb6327
dohye1 <tsc03018@gmail.com>
2021-02-16 21:44:57 +0900

    feat : postcontent component 생성

---
+++ src/components/PostContent/PostContent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as S from './PostContentStyle'
+
+function PostContent() {
+  return (
+    <S.Container>
+      <S.ContentBox>
+        <S.Title>asdfasdf</S.Title>
+        <S.Content>asdfasdfasd3f5sadf5sa6d5fs56d</S.Content>
+      </S.ContentBox>
+    </S.Container>
+  )
+}
+
+export default PostContent
---
+++ src/components/PostContent/PostContentStyle.ts
@@ -0,0 +1,36 @@
+import { Palette } from '@material-ui/icons'
+import styled from 'styled-components'
+import PALETTE from '../../styles/color-variables'
+import { BOX_STYLE } from '../../styles/common-style'
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 30px;
+  border-right: 1px solid ${PALETTE.INPUT_BORDER};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const ContentBox = styled.div`
+  box-shadow: ${BOX_STYLE.SHADOW};
+  background-color: ${PALETTE.WHITE};
+  border-radius: 10px;
+  width: 100%;
+`
+
+export const Title = styled.div`
+  width: 100%;
+  border-radius: 10px 10px 0 0;
+  padding: 20px;
+  font-size: 20px;
+  color: #7a7a7a;
+  border-bottom: 1px solid ${PALETTE.INPUT_BORDER};
+`
+export const Content = styled.div`
+  width: 100%;
+  border-radius: 10px 10px 0 0;
+  padding: 20px;
+  font-size: 15px;
+  color: ${PALETTE.INPUT_TEXT};
+`
