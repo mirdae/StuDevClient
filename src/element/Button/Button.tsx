ecc910a20c7898d382f5344865310c22b7b0b4b8
dohye1 <tsc03018@gmail.com>
2021-02-16 21:27:12 +0900

    feat : button element를 범용적으로 사용하기위해 props 수정

--- src/element/Button/Button.tsx
+++ src/element/Button/Button.tsx
@@ -3,12 +3,16 @@ import * as S from './ButtonStyle'
 
 type ButtonProps = {
   text: string
-  handleClick: () => void
-  type: 'submit'
+  handleClick?: () => void
+  buttonType: 'submit' | 'cancel'
 }
 
 function Button(props: ButtonProps) {
-  return <S.Container onClick={props.handleClick}>{props.text}</S.Container>
+  return (
+    <S.Container onClick={props.handleClick} buttonType={props.buttonType}>
+      {props.text}
+    </S.Container>
+  )
 }
 
 export default Button
--- src/element/Button/ButtonStyle.ts
+++ src/element/Button/ButtonStyle.ts
@@ -1,12 +1,24 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import PALETTE from '../../styles/color-variables'
 
-export const Container = styled.button`
-  padding: 0 70px;
+type ButtonProps = {
+  buttonType: 'submit' | 'cancel'
+}
+export const Container = styled.button<ButtonProps>`
+  padding: 0 50px;
   height: 50px;
   border-radius: 50px;
-  background-color: ${PALETTE.POINT};
-  color: ${PALETTE.WHITE};
-  font-size: 20px;
-  margin: 20px 0;
+  font-size: 15px;
+  margin: 20px 10px;
+  cursor: pointer;
+  ${(props) =>
+    props.buttonType === 'submit'
+      ? css`
+          background-color: ${PALETTE.POINT};
+          color: ${PALETTE.WHITE};
+        `
+      : css`
+          background-color: ${PALETTE.WHITE};
+          color: ${PALETTE.POINT};
+        `}
 `
