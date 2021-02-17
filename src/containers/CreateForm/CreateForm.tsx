d8c820f65db8dcff59ac86a538f0e6daa9a31048
dohye1 <tsc03018@gmail.com>
2021-02-17 15:05:52 +0900

    feat : form데이터 입력시 전역으로 관리

--- src/containers/CreateForm/CreateForm.tsx
+++ src/containers/CreateForm/CreateForm.tsx
@@ -1,11 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { useDispatch } from 'react-redux'
 import { CreateForm } from '../../components/CreateForm'
+import { changePostContent, changePostTitle } from '../../modules/post'
 
 function CreateFormContainer() {
-  const [title, setTitle] = useState<string>('')
-  const [content, setContent] = useState<string>('')
+  const dispatch = useDispatch()
+  const titleRef = useRef<HTMLInputElement>()
+  const contentRef = useRef<HTMLInputElement>()
 
-  return <CreateForm />
+  const handleTitleInput = () => {
+    console.log(titleRef.current?.value)
+    console.log(titleRef.current)
+    if (titleRef.current) {
+      dispatch(changePostTitle(titleRef.current.value))
+    }
+  }
+
+  const handleContentInput = () => {
+    console.log(contentRef.current?.value)
+    console.log(contentRef.current)
+    if (contentRef.current) {
+      dispatch(changePostContent(contentRef.current.value))
+    }
+  }
+  useEffect(() => {
+    console.log(titleRef.current)
+    titleRef.current?.addEventListener('focusout', handleTitleInput)
+    contentRef.current?.addEventListener('focusout', handleContentInput)
+    //return titleRef.current?.removeEventListener('focusout', handleTitleInput)
+  }, [])
+
+  return <CreateForm titleRef={titleRef} contentRef={contentRef} />
 }
 
 export default CreateFormContainer
