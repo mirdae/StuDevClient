3c65bdc51907010c104ce1f664e2aec8db585355
dohye1 <tsc03018@gmail.com>
2021-02-17 23:32:14 +0900

    feat : header props에 따라 헤더구성 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import * as S from './HeaderStyle'
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined'
 
-function Header() {
+type HeaderProps = {
+  isAuth: boolean
+  showMenu: boolean
+  isMain: boolean
+}
+
+function Header(props: HeaderProps) {
   return (
     <S.Container>
-      <S.Logo>StuDev</S.Logo>
+      <Link to="/">
+        <S.Logo>StuDev</S.Logo>
+      </Link>
+      {props.isMain && (
+        <S.Input>
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="검색어를 입력하세요" />
+        </S.Input>
+      )}
+      {props.isAuth && (
+        <Link to="/sign-in">
+          <S.Menu>Sign In</S.Menu>
+        </Link>
+      )}
     </S.Container>
   )
 }
--- src/components/Header/HeaderStyle.ts
+++ src/components/Header/HeaderStyle.ts
@@ -1,3 +1,4 @@
+import { Palette } from '@material-ui/icons'
 import styled from 'styled-components'
 import PALETTE from '../../styles/color-variables'
 
@@ -5,6 +6,8 @@ export const Container = styled.div`
   height: 60px;
   width: 100%;
   background-color: ${PALETTE.WHITE};
+  display: flex;
+  align-items: center;
 `
 
 export const Logo = styled.h3`
@@ -14,4 +17,31 @@ export const Logo = styled.h3`
   font-size: 24px;
   color: ${PALETTE.POINT};
   font-weight: 600;
+`
+
+export const Input = styled.div`
+  margin: 0 auto;
+  width: 300px;
+  height: 30px;
+  font-size: 15px;
+  border-radius: 50px;
+  color: ${PALETTE.INPUT_BORDER};
+  border: 1px solid ${PALETTE.INPUT_BORDER};
+  background-color: ${PALETTE.BACKGROUND};
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  input {
+    background: none;
+    padding: 0 10px;
+  }
+`
+
+export const Menu = styled.div`
+  position: fixed;
+  top: 20px;
+  right: 50px;
+  font-size: 1rem;
+  width: 8vw;
+  color: ${PALETTE.POINT};
 `
