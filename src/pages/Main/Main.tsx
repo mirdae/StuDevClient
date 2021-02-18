ce5dedb3a00f56a00653eb0be7cc5a5b56a5f6aa
dohye1 <tsc03018@gmail.com>
2021-02-18 15:29:52 +0900

    feat : main화면 render링 시 posts 데이터를 얻어오기

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,14 +1,24 @@
 import React, { useEffect } from 'react'
+import * as S from './MainStyle'
+import { useDispatch } from 'react-redux'
 import { Header } from '../../components/Header'
 import { useCookies } from 'react-cookie'
+import { requestGetAllPosts } from '../../modules/post'
+import { CardList } from '../../components/CardList'
 
 function Main() {
+  const dispatch = useDispatch()
   const [cookies, a, b] = useCookies(['auth'])
   useEffect(() => {
-    console.log(cookies.auth)
+    dispatch(requestGetAllPosts())
   }, [])
 
-  return <Header isAuth={!!cookies.auth} showMenu={true} isMain={true}></Header>
+  return (
+    <S.Container>
+      <Header isAuth={!!cookies.auth} showMenu={true} isMain={true}></Header>
+      <CardList />
+    </S.Container>
+  )
 }
 
 export default Main
