c3fbc57bd030cf182751a09452a14617287af59a
dohye1 <tsc03018@gmail.com>
2021-02-19 12:55:32 +0900

    feat : 로그인시 메인화면으로 전환

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useCookies } from 'react-cookie'
 import * as S from './SignInStyle'
 import { SignInForm } from '../../containers/SignInForm'
 import { Header } from '../../components/Header'
 
-function SignIn() {
+function SignIn(props: any) {
+  const userId = useSelector((state) => state.userReducer.id)
+  useEffect(() => {
+    console.log(userId)
+    if (userId) {
+      props.history.push('/')
+    }
+  })
+
   return (
     <S.Container>
       <Header isAuth={false} isMain={false} isSignPage={true} />
