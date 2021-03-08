f6058d28e14e25a72406472fd1c98bf9f2677478
dohye1 <tsc03018@gmail.com>
2021-03-08 23:08:27 +0900

    feat: 댓글 생성시 alert액션 dispatch

--- src/containers/CommentList/CommentList.tsx
+++ src/containers/CommentList/CommentList.tsx
@@ -1,12 +1,31 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { CommentList } from '../../components/CommentList'
+import { showAlert, hideAlert } from '../../modules/alert'
 import { RootState } from '../../modules'
 
 function CommentListContainer() {
+  const dispatch = useDispatch()
   const postComments = useSelector(
     (state: RootState) => state.postDetailReducer.comments
   )
+  const isMounted = useRef(false)
+
+  useEffect(() => {
+    console.log(isMounted.current, postComments.length)
+    if (!postComments.length) {
+      return
+    }
+    if (!isMounted.current) {
+      isMounted.current = true
+      return
+    }
+
+    dispatch(showAlert('댓글이 생성되었습니다.'))
+    setTimeout(() => {
+      dispatch(hideAlert(''))
+    }, 2000)
+  }, [postComments])
 
   return <CommentList comments={postComments} />
 }
