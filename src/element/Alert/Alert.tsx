971bc79dbfba725979aa51a79040c7aff86b3b62
dohye1 <tsc03018@gmail.com>
2021-03-08 23:07:49 +0900

    feat: alert 컴포넌트에 적절한 값 props로 전달

--- src/element/Alert/Alert.tsx
+++ src/element/Alert/Alert.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
+import { useSelector } from 'react-redux'
 import * as S from './AlertStyle'
 
-function Alert() {
-  return <S.Container>asdfasdf</S.Container>
+function Alert(): ReactElement {
+  const { message, isVisible } = useSelector((state) => state.alertReducer)
+  return <S.Container isVisible={isVisible}>{message}</S.Container>
 }
 
 export default Alert
--- src/element/Alert/AlertStyle.ts
+++ src/element/Alert/AlertStyle.ts
@@ -1,13 +1,29 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import PALETTE from '../../styles/color-variables'
 import { BOX_STYLE } from '../../styles/common-style'
 
-export const Container = styled.div`
-  width: 30vw;
+type VisibleType = {
+  isVisible: boolean
+}
+
+export const Container = styled.div<VisibleType>`
+  width: 25vw;
   padding: 20px 0;
   background-color: ${PALETTE.WHITE};
   border-radius: ${BOX_STYLE.RADIUS};
   box-shadow: ${BOX_STYLE.SHADOW};
   text-align: center;
   color: ${PALETTE.POINT};
+  position: fixed;
+  transition: 0.4s ease;
+  ${(props) =>
+    props.isVisible
+      ? css`
+          bottom: 10vh;
+          right: 10vw;
+        `
+      : css`
+          bottom: -20vh;
+          right: 10vw;
+        `}
 `
